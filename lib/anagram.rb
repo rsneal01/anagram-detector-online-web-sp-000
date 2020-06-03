c637ea7fab5cb79c48c6a386c97db7f97962bf07
Bobby Neal <rsneal01@gmail.com>
2020-06-03 16:25:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,10 +2,10 @@
 
 class Anagram
   
-  attr_accessor :word
+  attr_accessor :array_of_words
   
-  def initialize(word)
-    @word = word
+  def initialize(array_of_words)
+    @array_of_words = array_of_words
   end
   
   def match(array_of_words)
