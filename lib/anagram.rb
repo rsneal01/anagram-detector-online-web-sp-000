0acf17e7799cb2a21588ca3c7fc4f35cbbc4c5ac
Bobby Neal <rsneal01@gmail.com>
2020-06-03 16:59:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,8 +13,8 @@ class Anagram
   def match(array_of_words)
     anagram = @word.split("")
     array_of_words.each do |element|
-      element.split("")
-    # checks the new array to see if @word shares all same elements
+      element.split("").sort == anagram
+    end
   end
   
 end
